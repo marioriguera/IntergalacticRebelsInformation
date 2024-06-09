5bf46124e2ef90c153d7a8a99e6d6ab7bb03b3a8
Mario David Riguera Castillo <mario.riguera@gmail.com>
2024-06-09 16:48:50 +0200

    feat: principal api and angular web client app can run on a docker compose multicontainer.

---
+++ IRI.Clients.Web/src/environments/enviroment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  // otras configuraciones específicas para producción
+};
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'ITI.Clients.Web'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ITI.Clients.Web');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ITI.Clients.Web');
+  });
+});
